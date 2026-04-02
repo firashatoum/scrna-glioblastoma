56f0fccbbb8bbb7ce3b1aa6d38c43833dd904f06
Firas Hatoum <219299236+firashatoum@users.noreply.github.com>
2026-04-02 22:27:37 +0200

    pipeline: added test script

--- pipeline/test1.sh
+++ pipeline/test1.sh
@@ -1,5 +1,4 @@
-tail -n +2 config/sample_sheet.tsv | while IFS=$'\t' read -r sample_id gsm srr condition patient_id 
-fraction; do
+tail -n +2 config/sample_sheet.tsv | while IFS=$'\t' read -r sample_id gsm srr condition patient_id fraction; do
     echo "sample_id='${sample_id}'"
     break
 done
